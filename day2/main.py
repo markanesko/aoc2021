b09f1347e33068586e8b5916d55ee424bfd54867
mb <markoberic94@gmail.com>
2021-12-02 13:39:00 +0100

    adds parse_command

--- day2/main.py
+++ day2/main.py
@@ -5,13 +5,24 @@ cwd = os.getcwd()
 input_file = os.path.join(cwd, 'input.txt')
 
 
+def parse_command(command):
+
+    parsed = [
+        command[0],
+        int(command[1]),
+    ]
+
+    return parsed
+
+
 def create_commands():
 
     commands = []
 
     with open(input_file) as file:
         commands = file.readlines()
-        commands = [command.rstrip().split(' ') for command in commands]
+        commands = [parse_command(command.rstrip().split(' '))
+                    for command in commands]
 
     return commands
 
@@ -22,13 +33,16 @@ def run_no_aim(commands=[]):
     depth = 0
 
     for command in commands:
-        if 'forward' == command[0]:
-            horizontal += int(command[1])
+        instruction = command[0]
+        value = command[1]
+
+        if 'forward' == instruction:
+            horizontal += value
             continue
-        if 'down' == command[0]:
-            depth += int(command[1])
+        if 'down' == instruction:
+            depth += value
             continue
-        depth -= int(command[1])
+        depth -= value
 
     print(f'final position (h, d) = ({horizontal}, {depth})')
     print(f'final product is {horizontal * depth}')
@@ -44,7 +58,7 @@ def run_with_aim(commands=[]):
 
     for command in commands:
         instruction = command[0]
-        value = int(command[1])
+        value = command[1]
 
         if 'forward' == instruction:
             horizontal += value
