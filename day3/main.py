7c39a7c3772d28496e0cfc757d7b91ecd22eda3a
mb <markoberic94@gmail.com>
2021-12-06 11:33:46 +0100

    finishes day3

--- day3/main.py
+++ day3/main.py
@@ -18,20 +18,18 @@ def run():
     gama_rate = ''
     epsilon_rate = ''
 
-    dominant = []
 
-    for i in range(5):
+    for i in range(12):
         column = readings[:, i]
         num_ones = (column == '1').sum()
         num_zeroes = (column == '0').sum()
 
+
         if num_ones >= num_zeroes:
             gama_rate += '1'
             epsilon_rate += '0'
-            dominant.append('1')
             continue
         
-        dominant.append('0')
         gama_rate += '0'
         epsilon_rate += '1'
 
@@ -40,8 +38,52 @@ def run():
 
     print(f'product of gama and epsilon is {gama_rate * epsilon_rate}')
 
-    oxygen = []
-    co2 = []
+    oxygen = readings
+
+    for i in range(12):
+        
+        column = oxygen[:, i]
+        num_ones = (column == '1').sum()
+        num_zeros = abs(len(column) - num_ones)
+
+        d = '1'
+
+        if num_zeros > num_ones:
+            d = '0'
+
+        oxygen = oxygen[column == d, :]
+
+        if len(oxygen) == 1:
+            break     
+    
+    oxygen = ''.join(str(x) for x in oxygen[0])
+    oxygen = int(oxygen, 2)
+    print(oxygen)
+
+    co2 = readings
+
+    for i in range(12):
+        
+        column = co2[:, i]
+        num_ones = (column == '1').sum()
+        num_zeros = abs(len(column) - num_ones)
+
+        d = '0'
+
+        if num_ones < num_zeros:
+            d = '1'
+
+        co2 = co2[column == d, :]
+
+
+        if len(co2) == 1:
+            break        
+
+    co2 = ''.join(str(x) for x in co2[0])
+    co2 = int(co2, 2)
+    print(co2)
+
+    print(f'life supporting rating is {co2 * oxygen}')
 
     return 0
 
